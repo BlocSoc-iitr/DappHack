61afcc93203eaee9292c3fb9a55ba2ff08ec331a
me-loser <puspendramahariya@gmail.com>
2023-10-20 01:30:52 +0530

    frontend starter-kit

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import styles from "@/styles/page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <h1>This is home page.</h1>
+        </main>
+    );
+}
