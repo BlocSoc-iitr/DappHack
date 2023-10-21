02615bf57c9c0b16d4ca7aed435df70072708baa
Shashank Trivedi <shashanktrivedi1917@gmail.com>
2023-10-22 02:29:31 +0530

    pinata

---
+++ backend/routes/createNftRoute.js
@@ -0,0 +1,34 @@
+import createNftUri from "../pinata/createNftUri.js";
+import formidable from "formidable";
+const path = require("path");
+const fs = require("fs");
+
+export const createNftRoute = {
+  method: "post",
+  path: "/create-nft-uri",
+  handler: async (req, res) => {
+    const { file } = req.files;
+    file.mv("src/nftFiles/" + file.name, (err) => {
+      console.log("Upload Successful!");
+    });
+    const formData = {};
+    formData.name = req.body.name;
+    formData.description = req.body.description;
+    formData.traits = req.body.traits;
+    formData.filename = file.originalname;
+
+    console.log(
+      `NFT created: Name: ${formData.name}, Description: ${formData.description}, Traits: ${formData.traits}`
+    );
+
+    const response = await createNftUri(formData);
+    // const response = ["ipfs://QmUttGTuPbQkuroyGqrX3gS2b9SPdtYvyXLtxZjLHhgGuZ"];
+
+    console.log("here");
+    console.log(response);
+
+    res
+      .status(200)
+      .json({ message: "NFT created successfully", Uri: response });
+  },
+};
